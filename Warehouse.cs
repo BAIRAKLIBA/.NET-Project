ef4cce2d084d50ac9696069fe00c6f8cb7109ab5
BAIRAKLIBA <131465081+BAIRAKLIBA@users.noreply.github.com>
2025-07-28 01:34:13 +0400

    Update Warehouse.cs

--- Warehouse.cs
+++ Warehouse.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace project1.Models
 {
@@ -13,21 +9,36 @@ namespace project1.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
         public DateTime OperationDate { get; set; }
+
+        [Required, StringLength(50)]
         public string DocNumber { get; set; }
 
+        [Required, ForeignKey("Product")]
         public int ProductId { get; set; }
-        public virtual ProductCategory Product { get; set; }
+        public virtual Product Product { get; set; }
 
+        [Required, ForeignKey("Supplier")]
         public int SupplierId { get; set; }
         public virtual Supplier Supplier { get; set; }
 
+        [Required, ForeignKey("Unit")]
         public int UnitId { get; set; }
-        public virtual Unit Unit {get ;set;}
+        public virtual Unit Unit { get; set; }
 
-        public decimal Quantity { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+
         public DateTime? ExpiryDate { get; set; }
-        public decimal? UnitPrice { get; set; }
-        public decimal? RealizationPrice { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal")]
+        public decimal UnitPrice { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal")]
+        public decimal RealizationPrice { get; set; }
     }
 }
