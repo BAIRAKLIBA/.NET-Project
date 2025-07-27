1d85e3fcca36562ff670f0c6261a244517c685a9
BAIRAKLIBA <131465081+BAIRAKLIBA@users.noreply.github.com>
2025-07-28 01:30:41 +0400

    Update CustomerRelationship.cs

--- CustomerRelationship.cs
+++ CustomerRelationship.cs
@@ -1,11 +1,6 @@
-﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.Core.Metadata.Edm;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace project1.Models
 {
@@ -15,15 +10,20 @@ namespace project1.Models
         [Key]
         public int Id { get; set; }
 
+        [Required, ForeignKey("RelationshipType")]
         public int RelationshipTypeId { get; set; }
+    
         public virtual RelationshipType RelationshipType { get; set; }
 
+        [Required, ForeignKey("StartCustomer")]
         public int StartCustomerId { get; set; }
         public virtual Customer StartCustomer { get; set; }
 
+        [Required, ForeignKey("EndCustomer")]
         public int EndCustomerId { get; set; }
         public virtual Customer EndCustomer { get; set; }
 
+        [StringLength(500)]
         public string Comment { get; set; }
     }
 }
