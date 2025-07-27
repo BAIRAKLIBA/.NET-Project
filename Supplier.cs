5f563ffc78ed0e437bdcd01e3abd59d70bdb622f
BAIRAKLIBA <131465081+BAIRAKLIBA@users.noreply.github.com>
2025-07-28 01:33:33 +0400

    Update Supplier.cs

--- Supplier.cs
+++ Supplier.cs
@@ -1,33 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace project1.Models
 {
+    [Table("Suppliers")]
     public  class Supplier
     {
         [Key]
         public int Id { get; set; }
 
+        [Required, StringLength(50)]
         public string CompanyCode { get; set; }
 
-        [StringLength(100)]
+        [Required, StringLength(100)]
         public string CompanyName { get; set; }
 
-        [StringLength(100)]
-        public string ContactFullName { get; set; }
+        [Required, StringLength(100)]
+        public string CompanyFullName { get; set; }
 
-        public int? CityId { get; set; }
+        [Required]
+        public int CityId { get; set; }
         public virtual City City { get; set; }
 
-        public int? ContryId { get; set; }
-        public virtual Country Country { get; set; } 
+        [Required]
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
 
+        [StringLength(50)]
         public string Phone { get; set; }
+
+        [StringLength(50)]
         public string Email { get; set; }
+
+        [StringLength(100)]
         public string WebSite { get; set; }
         
         public virtual ICollection<Warehouse> Warehouses { get; set; }
