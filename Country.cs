803a7101393ea84059953bfdabb7ac158c7a0927
BAIRAKLIBA <131465081+BAIRAKLIBA@users.noreply.github.com>
2025-07-28 01:28:10 +0400

    Update Country.cs

--- Country.cs
+++ Country.cs
@@ -1,14 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace project1.Models
 {
+    [Table("Countries")]
     public class Country
     {
+        public Country()
+        {
+            Cities = new HashSet<City>();
+            Customers = new HashSet<Customer>();
+            Suppliers = new HashSet<Supplier>();
+        }
+
         [Key]
         public int Id { get; set; }
 
