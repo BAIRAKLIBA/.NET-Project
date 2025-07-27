aeec4d997683f64bf06e8a5711a1ccab2048bbce
BAIRAKLIBA <131465081+BAIRAKLIBA@users.noreply.github.com>
2025-07-28 01:43:33 +0400

    Add files via upload

---
+++ AddCustomerForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using project1.Models;
+using project1.Data;
+
+namespace project1.Forms
+{
+    public partial class AddCustomerForm : Form
+    {
+        private TextBox txtFirstName, txtLastName, txtPersonalNumber, txtEmail;
+        private ComboBox cbGender, cbCountry, cbCity;
+        private DateTimePicker dtpBirthDate;
+        private Button btnSave, btnCancel;
+        private ShopDbContext _context;
+
+        public AddCustomerForm()
+        {
+            _context = new ShopDbContext();
+            InitializeComponents();
+            LoadComboBoxes();
+        }
+
+        private void InitializeComponents()
+        {
+            this.Text = "ფიზიკური პირის დამატება";
+            this.Size = new System.Drawing.Size(400, 450);
+            this.StartPosition = FormStartPosition.CenterParent;
+
+            Label lblFirstName = new Label { Text = "სახელი", Top = 20, Left = 20 };
+            txtFirstName = new TextBox { Top = 40, Left = 20, Width = 300 };
+
+            Label lblLastName = new Label { Text = "გვარი", Top = 80, Left = 20 };
+            txtLastName = new TextBox { Top = 100, Left = 20, Width = 300 };
+
+            Label lblPersonalNumber = new Label { Text = "პირადი ნომერი", Top = 140, Left = 20 };
+            txtPersonalNumber = new TextBox { Top = 160, Left = 20, Width = 300 };
+
+            Label lblBirthDate = new Label { Text = "დაბადების თარიღი", Top = 200, Left = 20 };
+            dtpBirthDate = new DateTimePicker { Top = 220, Left = 20, Width = 300, Format = DateTimePickerFormat.Short };
+
+            Label lblGender = new Label { Text = "სქესი", Top = 260, Left = 20 };
+            cbGender = new ComboBox { Top = 280, Left = 20, Width = 300, DropDownStyle = ComboBoxStyle.DropDownList };
+
+            Label lblCountry = new Label { Text = "ქვეყანა", Top = 320, Left = 20 };
+            cbCountry = new ComboBox { Top = 340, Left = 20, Width = 300, DropDownStyle = ComboBoxStyle.DropDownList };
+
+            Label lblCity = new Label { Text = "ქალაქი", Top = 380, Left = 20 };
+            cbCity = new ComboBox { Top = 400, Left = 20, Width = 300, DropDownStyle = ComboBoxStyle.DropDownList };
+
+            Label lblEmail = new Label { Text = "ელ-ფოსტა", Top = 440, Left = 20 };
+            txtEmail = new TextBox { Top = 460, Left = 20, Width = 300 };
+
+            btnSave = new Button { Text = "დამატება", Top = 510, Left = 20, Width = 140 };
+            btnCancel = new Button { Text = "გაუქმება", Top = 510, Left = 180, Width = 140 };
+
+            btnSave.Click += BtnSave_Click;
+            btnCancel.Click += (s, e) => this.Close();
+
+            this.Controls.AddRange(new Control[]
+            {
+                lblFirstName, txtFirstName,
+                lblLastName, txtLastName,
+                lblPersonalNumber, txtPersonalNumber,
+                lblBirthDate, dtpBirthDate,
+                lblGender, cbGender,
+                lblCountry, cbCountry,
+                lblCity, cbCity,
+                lblEmail, txtEmail,
+                btnSave, btnCancel
+            });
+
+            this.Height = 620;
+        }
+
+        private void LoadComboBoxes()
+        {
+            cbGender.DataSource = _context.Genders.ToList();
+            cbGender.DisplayMember = "Name";
+            cbGender.ValueMember = "Id";
+
+            cbCountry.DataSource = _context.Countries.ToList();
+            cbCountry.DisplayMember = "Name";
+            cbCountry.ValueMember = "Id";
+
+            cbCity.DataSource = _context.Cities.ToList();
+            cbCity.DisplayMember = "Name";
+            cbCity.ValueMember = "Id";
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtLastName.Text) ||
+                string.IsNullOrWhiteSpace(txtPersonalNumber.Text))
+            {
+                MessageBox.Show("შეავსეთ ყველა სავალდებულო ველი!");
+                return;
+            }
+
+            var customer = new Customer
+            {
+                FirstName = txtFirstName.Text.Trim(),
+                LastName = txtLastName.Text.Trim(),
+                PersonalNumber = txtPersonalNumber.Text.Trim(),
+                BirthDate = dtpBirthDate.Value.Date,
+                GenderId = (int)cbGender.SelectedValue,
+                CountryId = (int)cbCountry.SelectedValue,
+                CityId = (int)cbCity.SelectedValue,
+                Email = txtEmail.Text.Trim()
+            };
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            MessageBox.Show("მომხმარებელი წარმატებით დაემატა!");
+            this.Close();
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace project1.Forms
+{
+    public partial class MainMenu : Form
+    {
+        private Button btnAdmin;
+        private Button btnProducts;
+        private Button btnSales;
+        private Button btnReports;
+
+        private ContextMenuStrip adminContextMenuStrip;
+        private ContextMenuStrip productsContextMenuStrip;
+        private ContextMenuStrip salesContextMenuStrip;
+        private ContextMenuStrip reportsContextMenuStrip;
+
+        public MainMenu()
+        {
+            InitializeMainMenuComponents();
+        }
+
+        private void InitializeMainMenuComponents()
+        {
+            this.Text = "Main Menu";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Size = new Size(800, 300);
+
+            // ---------- Buttons ----------
+
+            btnAdmin = new Button
+            {
+                Text = "ადმინისტრირება",
+                Location = new Point(10, 10),
+                Size = new Size(180, 30)
+            };
+            btnAdmin.Click += BtnAdmin_Click;
+
+            btnProducts = new Button
+            {
+                Text = "პროდუქტები",
+                Location = new Point(200, 10),
+                Size = new Size(180, 30)
+            };
+            btnProducts.Click += BtnProducts_Click;
+
+            btnSales = new Button
+            {
+                Text = "გაყიდვები",
+                Location = new Point(390, 10),
+                Size = new Size(180, 30)
+            };
+            btnSales.Click += BtnSales_Click;
+
+            btnReports = new Button
+            {
+                Text = "რეპორტები",
+                Location = new Point(580, 10),
+                Size = new Size(180, 30)
+            };
+            btnReports.Click += BtnReports_Click;
+
+            // ---------- Context Menus ----------
+
+            adminContextMenuStrip = new ContextMenuStrip();
+            adminContextMenuStrip.Items.Add("დამატება", null, (s, e) =>
+            {
+                // ფიზიკური პირის ფორმის გახსნა
+                var addCustomerForm = new AddCustomerForm();
+                addCustomerForm.ShowDialog();
+            });
+            adminContextMenuStrip.Items.Add("რედაქტირება", null, (s, e) => MessageBox.Show("რედაქტირება დაჭერილია"));
+            adminContextMenuStrip.Items.Add("წაშლა", null, (s, e) => MessageBox.Show("წაშლა დაჭერილია"));
+
+            productsContextMenuStrip = new ContextMenuStrip();
+            productsContextMenuStrip.Items.Add("დამატება", null, (s, e) => AddProduct());
+            productsContextMenuStrip.Items.Add("დასაწყობება", null, (s, e) => StoreProduct());
+            productsContextMenuStrip.Items.Add("რედაქტირება", null, (s, e) => EditProduct());
+            productsContextMenuStrip.Items.Add("წაშლა", null, (s, e) => DeleteProduct());
+            productsContextMenuStrip.Items.Add("ფასდაკლების მინიჭება", null, (s, e) => ApplyDiscount());
+            productsContextMenuStrip.Items.Add("ჩამოწერა", null, (s, e) => WriteOffProduct());
+
+            salesContextMenuStrip = new ContextMenuStrip();
+            salesContextMenuStrip.Items.Add("პროდუქტის გაყიდვა", null, (s, e) => SellProduct());
+            salesContextMenuStrip.Items.Add("გაყიდული პროდუქტები", null, (s, e) => ShowSoldProducts());
+
+            reportsContextMenuStrip = new ContextMenuStrip();
+            reportsContextMenuStrip.Items.Add("ფიზიკური პირების სია", null, (s, e) => ShowIndividualsList());
+            reportsContextMenuStrip.Items.Add("პროდუქტის რეალიზაციის სტატისტიკა ქალაქების მიხედვით", null, (s, e) => ShowSalesStatsByCity());
+            reportsContextMenuStrip.Items.Add("Top 3 ყველაზე მოთხოვნადი სპორტული ინვენტარი", null, (s, e) => ShowTop3SportsInventory());
+            reportsContextMenuStrip.Items.Add("ძვირადღირებული წიგნები", null, (s, e) => ShowExpensiveBooks());
+            reportsContextMenuStrip.Items.Add("3 წლამდე ბავშვებისთვის რეკომენდირებული სათამაშოები", null, (s, e) => ShowRecommendedToys());
+
+            // ---------- Add buttons to form ----------
+            this.Controls.Add(btnAdmin);
+            this.Controls.Add(btnProducts);
+            this.Controls.Add(btnSales);
+            this.Controls.Add(btnReports);
+        }
+
+        // ---------- Button Click Handlers ----------
+
+        private void BtnAdmin_Click(object sender, EventArgs e)
+        {
+            adminContextMenuStrip.Show(btnAdmin, new Point(0, btnAdmin.Height));
+        }
+
+        private void BtnProducts_Click(object sender, EventArgs e)
+        {
+            productsContextMenuStrip.Show(btnProducts, new Point(0, btnProducts.Height));
+        }
+
+        private void BtnSales_Click(object sender, EventArgs e)
+        {
+            salesContextMenuStrip.Show(btnSales, new Point(0, btnSales.Height));
+        }
+
+        private void BtnReports_Click(object sender, EventArgs e)
+        {
+            reportsContextMenuStrip.Show(btnReports, new Point(0, btnReports.Height));
+        }
+
+        // ---------- Products menu methods ----------
+
+        private void AddProduct() => MessageBox.Show("დამატება დაჭერილია (პროდუქტები)");
+        private void StoreProduct() => MessageBox.Show("დასაწყობება დაჭერილია");
+        private void EditProduct() => MessageBox.Show("რედაქტირება დაჭერილია (პროდუქტები)");
+        private void DeleteProduct() => MessageBox.Show("წაშლა დაჭერილია (პროდუქტები)");
+        private void ApplyDiscount() => MessageBox.Show("ფასდაკლების მინიჭება დაჭერილია");
+        private void WriteOffProduct() => MessageBox.Show("ჩამოწერა დაჭერილია");
+
+        // ---------- Sales menu methods ----------
+
+        private void SellProduct() => MessageBox.Show("პროდუქტის გაყიდვა დაჭერილია");
+        private void ShowSoldProducts() => MessageBox.Show("გაყიდული პროდუქტები დაჭერილია");
+
+        // ---------- Reports menu methods ----------
+
+        private void ShowIndividualsList() => MessageBox.Show("ფიზიკური პირების სია");
+        private void ShowSalesStatsByCity() => MessageBox.Show("სტატისტიკა ქალაქების მიხედვით");
+        private void ShowTop3SportsInventory() => MessageBox.Show("Top 3 სპორტული ინვენტარი");
+        private void ShowExpensiveBooks() => MessageBox.Show("ძვირადღირებული წიგნები");
+        private void ShowRecommendedToys() => MessageBox.Show("3 წლამდე ბავშვებისთვის სათამაშოები");
+    }
+}
